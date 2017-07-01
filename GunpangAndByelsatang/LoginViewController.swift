f4b2f2effc03ec38f79ed0ce95becd7e23919b0c
EJSohn <5on.3un.7oo@gmail.com>
2017-07-02 01:36:33 +0900

    add login viewcontroller some ui variables and actions

--- GunpangAndByelsatang/LoginViewController.swift
+++ GunpangAndByelsatang/LoginViewController.swift
@@ -8,11 +8,29 @@
 
 import UIKit
 
+
+/**
+ Initial login view controller.
+ */
+
 class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var kakaoLoginBtn: UIButton!
+    @IBOutlet weak var facebookLoginBtn: UIButton!
+    @IBOutlet weak var googleLoginBtn: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+    }
+    
+    @IBAction func didPressKakaoBtn(_ sender: UIButton) {
+    }
+    
+    @IBAction func didPressFacebookBtn(_ sender: UIButton) {
+    }
+
+    @IBAction func didPressGoogleBtn(_ sender: UIButton) {
     }
 
 }
