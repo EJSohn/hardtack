46b1df8a530a2440a7a46aa57c5c53a72f832059
EJSohn <5on.3un.7oo@gmail.com>
2017-07-02 01:33:47 +0900

    add login view some constraints

---
+++ GunpangAndByelsatang/LoginViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LoginViewController.swift
+//  GunpangAndByelsatang
+//
+//  Created by 손은주 on 2017. 7. 2..
+//  Copyright © 2017년 Ejsohn. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
