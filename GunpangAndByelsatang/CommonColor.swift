814cd3b53ff00b2a1727f8c66b1e6dda4da478d7
EJSohn <5on.3un.7oo@gmail.com>
2017-07-02 21:19:38 +0900

    Use commonColor's get, set properly

--- GunpangAndByelsatang/CommonColor.swift
+++ GunpangAndByelsatang/CommonColor.swift
@@ -15,39 +15,24 @@ import Foundation
  */
 class CommonColor {
     
-    static let shared = CommonColor(with: nil)
+    static let shared = CommonColor()
+    
+    private var _main: UIColor = UIColor(red: 178/255, green: 255/255, blue: 89/255, alpha: 1)
     
     public var main: UIColor {
         get {
             
-            return self.main
+            return _main
         }
         
         set(color){
-            self.main = color
+            if color != UIColor.white {
+                _main = color
+            } else {
+                log.warning("Main color can't be white.")
+            }
         }
     }
     
-    public var sub: UIColor {
-        get {
-            return self.sub
-        }
-        
-        set(color){
-            self.sub = color
-        }
-    }
-    
-    init(
-        with main:UIColor?,
-        and sub: UIColor = UIColor.white) {
-        
-        if let main = main {
-            self.main = main
-        } else {
-            // default color (hex #b2ff59)
-            self.main = UIColor(red: 178/255, green: 255/255, blue: 89/255, alpha: 1)
-        }
-        self.sub = sub
-    }
+    init() { }
 }
